454487206571ae8f0f6ca89b68f0c4119a1bc470
Arash Emami <aemami99@gmail.com>
2013-11-11 16:16:24 -0800

    added web app

---
+++ BLT.Sandbox/Sandbox/Sandbox.WebApp/Controllers/ClientsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sandbox.Data;
+using Sandbox.Data.Entity;
+
+namespace Sandbox.WebApp.Controllers
+{
+    public class ClientsController : Controller
+    {
+        DataContext db = new DataContext();
+
+        // GET: /Clients/
+        public async Task<ActionResult> Index()
+        {
+            var clients = await db.Clients.ToListAsync();
+            return View(clients);
+        }
+
+        // GET: /Clients/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = await db.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // GET: /Clients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Clients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="Id,Name,LogoUrl,ProjectCount,LatestProjectTime,LatestProjectName,LatestProjectId")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                client.Id = Guid.NewGuid();
+                db.Clients.Add(client);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(client);
+        }
+
+        // GET: /Clients/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = await db.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: /Clients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="Id,Name,LogoUrl,ProjectCount,LatestProjectTime,LatestProjectName,LatestProjectId")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(client);
+        }
+
+        // GET: /Clients/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = await db.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: /Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            Client client = await db.Clients.FindAsync(id);
+            db.Clients.Remove(client);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BLT.Sandbox/Sandbox/Sandbox.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sandbox.WebApp.Startup))]
+namespace Sandbox.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

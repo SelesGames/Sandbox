e937791d44994d55df276ee86dc91ce7241149d6
ericsarvey <ericsarvey@gmail.com>
2013-11-26 21:59:59 -0800

    Campaign/Details.cshtml layout, better text overlay with jquery

---
+++ BLT.Sandbox/Sandbox/Sandbox.WebApp/Scripts/text-overlay.js
@@ -0,0 +1,20 @@
+﻿function textOverlay() {
+    $('a.fill-div').each(function (idx) {
+        var left = $(this).position().left;
+        var top = $(this).position().top;
+        var extraDelta = $(this).height() / 2 - 18;
+        var width = $(this).width();
+        $(this).find('.text-overlay').css('width', width).css('top', top + extraDelta).css('left', left);
+    });
+    $('.text-overlay').show();
+}
+
+$(function () {
+
+    textOverlay();
+    $(window).resize(function () {
+        $('.text-overlay').hide();
+        textOverlay();
+    });
+
+});
